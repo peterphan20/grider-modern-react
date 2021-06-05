eaad30ccbc0a2f93aa9c5c96c1ce2137d51db9b6
Peter Phan <peterphan0422@gmail.com>
2021-06-05 17:29:22 -0500

    bug

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from "../apis/streams";
+import history from "../history";
 import {
 	SIGN_IN,
 	SIGN_OUT,
@@ -27,13 +28,12 @@ export const createStream = (formValues) => async (dispatch, getState) => {
 	const response = await streams.post("/streams", { ...formValues, userId });
 
 	dispatch({ type: CREATE_STREAM, payload: response.data });
-	// Do some programmatic navigation to
-	// get the user back to the root route
+	history.push("/");
 };
 
 export const fetchStreams = () => async (dispatch) => {
 	const response = await streams.get("/streams");
-
+	console.log(response.data);
 	dispatch({ type: FETCH_STREAMS, payload: response.data });
 };
 
