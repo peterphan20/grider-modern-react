2ddae1aa5ca5f9b863d45d1283f3461e6f66a749
Peter Phan <peterphan0422@gmail.com>
2021-06-09 20:50:00 -0500

    feat: translate boiler plating

---
+++ translate/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import UserCreate from "./components/UserCreate";
+
+class App extends React.Component {
+	state = { language: "english" };
+
+	onLanguageChange = (language) => {
+		this.setState({ language });
+	};
+
+	render() {
+		return (
+			<div className="ui container">
+				<div>
+					Select a language
+					<i className="flag us" onClick={() => this.onLanguageChange("english")} />
+					<i className="flag nl" onClick={() => this.onLanguageChange("dutch")} />
+				</div>
+				<UserCreate />
+			</div>
+		);
+	}
+}
+
+export default App;
