d23d1a47ae00f15c3b736d734dfc581fce5859c8
Peter Phan <peterphan0422@gmail.com>
2021-06-10 16:55:36 -0500

    feat: refactor to context system

--- translate/src/App.js
+++ translate/src/App.js
@@ -1,25 +1,19 @@
 import React from "react";
 import UserCreate from "./components/UserCreate";
-import LanguageContext from "./contexts/LanguageContext";
+import LanguageSelector from "./components/LanguageSelector";
+import { LanguageStore } from "./contexts/LanguageContext";
+import ColorContext from "./contexts/ColorContext";
 
 class App extends React.Component {
-	state = { language: "english" };
-
-	onLanguageChange = (language) => {
-		this.setState({ language });
-	};
-
 	render() {
 		return (
 			<div className="ui container">
-				<div>
-					Select a language
-					<i className="flag us" onClick={() => this.onLanguageChange("english")} />
-					<i className="flag nl" onClick={() => this.onLanguageChange("dutch")} />
-				</div>
-				<LanguageContext.Provider value={this.state.language}>
-					<UserCreate />
-				</LanguageContext.Provider>
+				<LanguageStore>
+					<LanguageSelector />
+					<ColorContext.Provider value="red">
+						<UserCreate />
+					</ColorContext.Provider>
+				</LanguageStore>
 			</div>
 		);
 	}
--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -1,19 +1,26 @@
 import React from "react";
 import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
 
 class Button extends React.Component {
 	// static adds a property to our class itself
 	// static contextType = LanguageContext;
 
-	renderSubmit(value) {
-		return value === "english" ? "Submit" : "Voorleggen";
+	renderSubmit(language) {
+		return language === "english" ? "Submit" : "Voorleggen";
 	}
 
 	render() {
 		return (
-			<button className="ui button primary">
-				<LanguageContext.Consumer>{(value) => this.renderSubmit(value)}</LanguageContext.Consumer>
-			</button>
+			<ColorContext.Consumer>
+				{(color) => (
+					<button className={`ui button ${color}`}>
+						<LanguageContext.Consumer>
+							{({ language }) => this.renderSubmit(language)}
+						</LanguageContext.Consumer>
+					</button>
+				)}
+			</ColorContext.Consumer>
 		);
 	}
 }
